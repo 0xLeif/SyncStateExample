ca337abe631429ac23d6152148c71c2e4c8a3e4d
Leif <zmeriksen@icloud.com>
2024-03-01 18:14:16 -0700

    Initial Commit

---
+++ SyncStateExample/SyncStateExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SyncStateExampleApp.swift
+//  SyncStateExample
+//
+//  Created by Leif on 3/1/24.
+//
+
+import SwiftUI
+
+@main
+struct SyncStateExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
