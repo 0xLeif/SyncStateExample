75fb1f865fe6ff3fbcf064d07885b7b38610b47f
Leif <zmeriksen@icloud.com>
2024-03-01 18:35:50 -0700

    init

--- SyncStateExample/ContentView.swift
+++ SyncStateExample/ContentView.swift
@@ -5,17 +5,64 @@
 //  Created by Leif on 3/1/24.
 //
 
+import AppState
 import SwiftUI
 
+struct Settings: Codable {
+    var text: String
+    var isShowingSheet: Bool
+    var isDarkMode: Bool
+}
+
+extension Application {
+    var settings: SyncState<Settings> {
+        syncState(
+            initial: Settings(
+                text: "Hello, World!",
+                isShowingSheet: false,
+                isDarkMode: false
+            ),
+            id: "settings"
+        )
+    }
+}
+
+class CustomApplication: Application {
+    override func didChangeExternally(notification: Notification) {
+        super.didChangeExternally(notification: notification)
+
+        DispatchQueue.main.async {
+            self.objectWillChange.send()
+        }
+    }
+}
+
 struct ContentView: View {
+    @SyncState(\.settings) private var settings: Settings
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            TextField("", text: $settings.text)
+
+            Button(settings.isDarkMode ? "Light" : "Dark") {
+                settings.isDarkMode.toggle()
+            }
+
+            Button("Show") { settings.isShowingSheet = true }
         }
-        .padding()
+        .preferredColorScheme(settings.isDarkMode ? .dark : .light)
+        .sheet(isPresented: $settings.isShowingSheet, content: ContentViewInnerView.init)
+    }
+}
+
+struct ContentViewInnerView: View {
+    @Slice(\.settings, \.text) private var text: String
+
+    var body: some View {
+        Text("\(text)")
+            .onTapGesture {
+                text = Date().formatted()
+            }
     }
 }
 
--- SyncStateExample/SyncStateExampleApp.swift
+++ SyncStateExample/SyncStateExampleApp.swift
@@ -5,10 +5,18 @@
 //  Created by Leif on 3/1/24.
 //
 
+import AppState
 import SwiftUI
 
 @main
 struct SyncStateExampleApp: App {
+    init() {
+        Application
+            .promote(to: CustomApplication.self)
+            .logging(isEnabled: true)
+            .load(dependency: \.icloudStore)
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
